4bfed3ea13077f0380981ea507aaf9ce79fc79e4
Matt Clark <reddevdesign@gmail.com>
2022-05-02 15:38:46 -0400

    more setup

--- plugins/components.js
+++ plugins/components.js
@@ -1,28 +1,6 @@
-import Vue from 'vue'
-import Page from '~/components/Page.vue'
-import Test from '~/components/blocks/Test.vue'
-import HighlightCta from '~/components/blocks/HighlightCta.vue'
-import InteriorTitle from '~/components/blocks/InteriorTitle.vue'
-import HomepageHero from '~/components/blocks/HomepageHero.vue'
-import TwoColImageRightCta from '~/components/blocks/TwoColImageRightCta'
-import CareCard from '~/components/blocks/CareCard'
-import TwoColImage from '~/components/blocks/TwoColImage'
-import ThreeUpCard from '~/components/blocks/ThreeUpCard'
-import ClientTestimonial from '~/components/blocks/ClientTestimonial'
-import TreatmentModalities from '~/components/blocks/TreatmentModalities'
-import ThreeColText from '~/components/blocks/ThreeColText'
-import ImageHighlight from '~/components/blocks/ImageHighlight'
+import Vue from "vue";
+import Page from "~/components/Page.vue";
+import Test from "~/components/blocks/Test.vue";
 
-Vue.component('page', Page)
-Vue.component('test', Test)
-Vue.component('HighlightCta', HighlightCta)
-Vue.component('InteriorTitle', InteriorTitle)
-Vue.component('HomepageHero', HomepageHero)
-Vue.component('TwoColImageRightCta', TwoColImageRightCta)
-Vue.component('CareCard', CareCard)
-Vue.component('TwoColImage', TwoColImage)
-Vue.component('ThreeUpCard', ThreeUpCard)
-Vue.component('ClientTestimonial', ClientTestimonial)
-Vue.component('TreatmentModalities', TreatmentModalities)
-Vue.component('ThreeColText', ThreeColText)
-Vue.component('ImageHighlight', ImageHighlight)
+Vue.component("page", Page);
+Vue.component("test", Test);
