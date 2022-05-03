2e40ee266fc5f4e8694ac3ba184403425b0be886
Matt Clark <reddevdesign@gmail.com>
2022-05-03 18:55:39 -0400

    messing with storybook -M

--- components/blocks/Test.vue
+++ components/blocks/Test.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
+  <div :class="`${block.size} ${block.color}`">
     {{ block.label }}
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      block: {
-        type: Object,
-        required: true,
-      },
+export default {
+  props: {
+    block: {
+      type: Object,
+      required: true,
     },
-  }
+  },
+};
 </script>
+
+<style lang="postcss" scoped>
+.h1 {
+  @apply text-7xl;
+}
+
+.h2 {
+  @apply text-5xl;
+}
+
+.h3 {
+  @apply text-4xl;
+}
+
+.h4 {
+  @apply text-xl;
+}
+
+.blue {
+  @apply text-blue-500;
+}
+
+.green {
+  @apply text-green-500;
+}
+
+.red {
+  @apply text-red-500;
+}
+</style>
