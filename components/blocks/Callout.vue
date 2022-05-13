80346d9f4aa9df02f32ec6be7ee930311edcfbe9
Drew <drewclementsdesign@gmail.com>
2022-05-13 16:42:00 -0400

    should work. no idea why it isnt

---
+++ components/blocks/Callout.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <!-- <BaseImage
+      src="../../assets/images/NicePng_baseball-png_36804 1.png"
+      alt=""
+    /> -->
+    <BaseHeading size="h2">{{ block.heading }}</BaseHeading>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    block: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Page from "~/components/Page.vue";
 import Test from "~/components/blocks/Test.vue";
+import Callout from "~/components/blocks/Callout.vue";
 
 Vue.component("page", Page);
 Vue.component("test", Test);
+Vue.component("callout", Callout);
