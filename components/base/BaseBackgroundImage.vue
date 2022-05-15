a488dff5bea724236dbb58d4fb6c373e07fbd2e9
Drew <drewclementsdesign@gmail.com>
2022-05-15 17:25:15 -0400

    update focus color

--- components/base/BaseBackgroundImage.vue
+++ components/base/BaseBackgroundImage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="absolute inset-0 width-full height-full overflow-hidden">
+  <div
+    class="
+      absolute
+      inset-0
+      width-full
+      height-full
+      overflow-hidden
+      pointer-events-none
+    "
+  >
     <div
       class="absolute inset-0 width-full height-full bg-primary-dark opacity-75"
     />
