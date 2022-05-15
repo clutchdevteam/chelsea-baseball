9f8a028f5cef5c1bf326eb2da784cabdb2f63596
Drew <drewclementsdesign@gmail.com>
2022-05-15 18:46:30 -0400

    bing bong boom

--- plugins/components.js
+++ plugins/components.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import Page from "~/components/Page.vue";
 import TwoColImageLeft from "~/components/blocks/TwoColImageLeft.vue";
 import CallOut from "~/components/blocks/CallOut.vue";
+import Highlight from "~/components/blocks/Highlight.vue";
 
 Vue.component("page", Page);
 Vue.component("TwoColImageLeft", TwoColImageLeft);
 Vue.component("CallOut", CallOut);
+Vue.component("Highlight", Highlight);
