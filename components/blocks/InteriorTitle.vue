255550464347b0dff3496214bcfd8ec75f289892
Drew <drewclementsdesign@gmail.com>
2022-05-23 14:03:15 -0400

    fix conditional check

--- components/blocks/InteriorTitle.vue
+++ components/blocks/InteriorTitle.vue
@@ -13,7 +13,7 @@
 
     <div
       class="relative desktop-container -mt-24 lg:-mt-12 z-40"
-      v-if="block.rosterImage.length"
+      v-if="block.rosterImage.filename.length"
     >
       <BaseImage
         class="w-full lg:w-1/2 mx-auto"
