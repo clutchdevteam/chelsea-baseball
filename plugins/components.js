066ca7827c5de7922eacfdba8aba90f005c331e3
Drew <drewclementsdesign@gmail.com>
2022-05-23 14:13:19 -0400

    minute entry

--- plugins/components.js
+++ plugins/components.js
@@ -6,6 +6,7 @@ import CallOut from "~/components/blocks/CallOut.vue";
 import Highlight from "~/components/blocks/Highlight.vue";
 import HomepageHero from "~/components/blocks/HomepageHero.vue";
 import InteriorTitle from "~/components/blocks/InteriorTitle.vue";
+import MinuteEntry from "~/components/blocks/MinuteEntry.vue";
 
 Vue.component("page", Page);
 Vue.component("Interior", Interior);
@@ -14,3 +15,4 @@ Vue.component("CallOut", CallOut);
 Vue.component("Highlight", Highlight);
 Vue.component("HomepageHero", HomepageHero);
 Vue.component("InteriorTitle", InteriorTitle);
+Vue.component("MinuteEntry", MinuteEntry);
