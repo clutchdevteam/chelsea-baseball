83af91505e1ffee66c70a760281076572023719a
Matt Clark <reddevdesign@gmail.com>
2022-05-30 12:52:56 -0400

    built NewsCard atom and NewsUpdates component

--- plugins/components.js
+++ plugins/components.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import Page from "~/components/Page.vue";
 import TwoColImageLeft from "~/components/blocks/TwoColImageLeft.vue";
 import CallOut from "~/components/blocks/CallOut.vue";
+import NewsCard from "~/components/atoms/NewsCard";
+import NewsUpdates from "~/components/blocks/NewsUpdates";
 
 Vue.component("page", Page);
 Vue.component("TwoColImageLeft", TwoColImageLeft);
 Vue.component("CallOut", CallOut);
+Vue.component("NewsCard", NewsCard);
+Vue.component("NewsUpdates", NewsUpdates);
